4c44d6e0c1211b2af6531ec1a153402ecf4c198c
francijpg <hellofrancijpg@gmail.com>
2022-10-30 13:18:50 -0300

    chore: create the sync sorted solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,29 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // step 1: create a empty array to contain the log entries
+  const logs = [];
+
+  // step 2: navigate through each list to add each record found in the logs array created in step 1
+  for (let i = 0; i < logSources.length; i++) {
+    const logSource = logSources[i];
+    let logEntry = logSource.pop();
+    while (logEntry) {
+      logs.push(logEntry);
+      logEntry = logSource.pop();
+    }
+  }
+
+  // step 3: sort the list of logs by chronological date
+  let logsSorted = logs.sort((a, b) => a.date - b.date);
+
+  // step 4: loop through each log in the list to be able to print them
+  for (let i = 0; i < logsSorted.length; i++) {
+    printer.print(logsSorted[i]);
+  }
+
+  // step 5: print the results
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
